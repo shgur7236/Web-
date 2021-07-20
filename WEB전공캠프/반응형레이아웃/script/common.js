ae9df867175f237858fab92184ac89d22a52b4be
Noh-hyuck <81364498+shgur7236@users.noreply.github.com>
2021-07-20 22:41:06 +0900

    Add files via upload

---
+++ WEB전공캠프/반응형레이아웃/script/common.js
@@ -0,0 +1,42 @@
+$(function() {
+
+    var idx=0;
+
+    setInterval(function() {
+        if(idx<2){
+            idx++;
+        }else {
+            idx=0;
+        }
+        console.log(idx);
+
+        $("#content3 img").eq(idx).fadeIn();
+        $("#content3 img").eq(idx-1).fadeOut();
+    },2000);
+
+    $("#m_btn").click(function(e) {
+        e.preventDefault();
+
+        if($("nav").is(":visible")){
+            $("nav").hide();
+            $("#m_btn").removeClass("on");
+
+        } else {
+        
+            $("nav").show();
+            $("#m_btn").addClass("on");
+
+        } 
+    });
+
+    $("nav>ul>li>a").click(function(e) {
+        e.preventDefault();
+
+        $(this).next("ul").slideToggle();
+       /* if($(this).next("ul").is(":visible")) {
+            $(this).next("ul").slideUp();
+        } else {
+            $(this).next("ul").slideDown();
+        }*/
+    });
+});
